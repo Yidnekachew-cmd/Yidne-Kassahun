d33773c2d580f0152e749e9200665673153b5110
Yidne <yamiyekn@gmail.com>
2024-01-14 23:48:51 +0300

    Modifiy project card images and size of the cards.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,9 +2,9 @@ import PropTypes from "prop-types";
 // import { Carousel } from 'react-responsive-carousel';
 // import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import '../index.css'
-import project1 from '../assets/project-1.png'
-import project2 from '../assets/project-2.png'
-import project3 from '../assets/project-3.png'
+import project1 from '../assets/MathMag.png'
+import project2 from '../assets/PopIndex.png'
+import project3 from '../assets/netflix.png'
 
 const Projects = ({
   // handleGitHubIconClick,
@@ -21,12 +21,12 @@ const Projects = ({
       <div className="">
         <div className="flex flex-col justify-center items-start lg:flex-row md:items-center gap-10 lg:gap-8 mt-8">
         {/* <Carousel showThumbs={true} showStatus={false} showIndicators={false} showArrows={true} centerMode> */}
-          <div className="rounded-3xl p-6 border-2 border-[#a3a3a3] space-y-3 shadow-2xl pb-12 bg-[fafafa]">
-            <div className="flex justify-center ">
+          <div className="rounded-3xl lg:w-[32%] h-auto px-6 md:pt-6 lg:pt-0  border-2 border-[#a3a3a3] space-y-3 shadow-2xl pb-12 bg-[fafafa]">
+            <div className="flex justify-center h-64">
               <img
                 src={project1}
                 alt="Project 1"
-                className="w-[90%] h-[90%] rounded-3xl pb-4"
+                className="w-auto h-full object-contain rounded-3xl "
               />
             </div>
             <h2 className="text-center text-3xl font-bold pb-2">Maths Magician</h2>
@@ -45,12 +45,12 @@ const Projects = ({
               </button>
             </div>
           </div>
-          <div className="rounded-3xl p-6 border-2 border-[#a3a3a3] space-y-3 shadow-2xl pb-12 bg-[fafafa]">
-            <div className="flex justify-center">
+          <div className="rounded-3xl lg:w-[32%] h-auto px-6 md:pt-6 lg:pt-0   border-2 border-[#a3a3a3] space-y-3 shadow-2xl pb-12 bg-[fafafa]">
+            <div className="flex justify-center h-64">
               <img
                 src={project2}
                 alt="Project 2"
-                className="w-[90%] h-[90%] rounded-3xl pb-4"
+                className="w-auto h-full object-contain rounded-3xl pb-4"
               />
             </div>
             <h2 className="text-center text-3xl font-bold pb-2">Population Index</h2>
@@ -69,15 +69,15 @@ const Projects = ({
               </button>
             </div>
           </div>
-          <div className="rounded-3xl p-6 border-2 border-[#a3a3a3] space-y-3 shadow-2xl pb-12 bg-[fafafa]">
-            <div className="flex justify-center">
+          <div className="rounded-3xl lg:w-[32%] h-auto px-6 md:pt-6 lg:pt-0 border-2 border-[#a3a3a3] space-y-3 shadow-2xl pb-12 bg-[fafafa]">
+            <div className="flex justify-center h-64">
               <img
                 src={project3}
                 alt="Project 3"
-                className="w-[90%] h-[90%] rounded-3xl pb-4"
+                className="w-auto h-full object-contain rounded-3xl pb-4"
               />
             </div>
-            <h2 className="text-center text-3xl font-bold pb-2">Todo App</h2>
+            <h2 className="text-center text-3xl font-bold pb-2">Netflix Clone</h2>
             <div className="flex gap-4 items-center justify-center">
               <button
                 className="bg-white text-xl text-[#A53DFF] border-2 border-[#A53DFF] hover:bg-[#A53DFF] transition-colors duration-300 hover:text-white font-bold rounded-3xl px-5 py-3"
