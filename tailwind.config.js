848231eb187c74c5c6953e13f3e59a6097c850e0
Yidne <yamiyekn@gmail.com>
2024-01-14 13:15:05 +0300

    Add tailwind configuration.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-bg': "url('./assets/Homepage.jpg')",
+        'footer-texture': "url('./assets/images/footer-texture.png')",
+      },
+      },
   },
   plugins: [],
 }
