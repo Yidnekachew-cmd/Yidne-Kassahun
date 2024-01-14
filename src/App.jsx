8324d2be90392f74f4b5c44cc4c6ad63393f40c2
Yidne <yamiyekn@gmail.com>
2024-01-14 13:08:00 +0300

    Create App component and add routes and states.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import './index.css';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
@@ -6,22 +6,110 @@ import About from './components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 
 
 function App() {
+  const navigate = useNavigate();
+
+  const handleContactClick = () => {
+    navigate('./contact');
+  };
+
+  const handleDownloadClick = () => {
+    const resume = 'https://drive.google.com/file/d/1QgvkSTkEmd-LYfccSXNRb-3gaA2ZC-tr/view?usp=sharing';
+    window.open(resume, '_blank');
+  };
+
+  const handleLinkedinIconClick = () => {
+    const linkedin = 'https://www.linkedin.com/in/yidnekachew-kassahun/';
+    window.open(linkedin, '_blank');
+  };
+
+  const handleGitHubIconClick = () => {
+    
+    const gitHub = 'https://github.com/Yidnekachew-cmd';
+    window.open(gitHub, '_blank');
+  };
+
+  const handleEmailIconClick = () => {
+    
+    const email = 'mailto:yamiyekn@gmail.com';
+    window.open(email, '_blank');
+  };
+
+  const handleClickMathGitHub = () => {
+    
+    const math = 'https://github.com/Yidnekachew-cmd/math-magician';
+    window.open(math, '_blank');
+  };
+  const handleClickPopGitHub = () => {
+    
+    const pop = 'https://github.com/Yidnekachew-cmd/Popuation-Index';
+    window.open(pop, '_blank');
+  };
+
+  // const handleClickTodoGitHub = () => {
+    
+  //   const todo = 'https://github.com/Yidnekachew-cmd/math-magician';
+  //   window.open(todo, '_blank');
+  // };
+
+  const handleClickMathLive = () => {
+    
+    const mathLive = 'https://math-magician-yidnekachew-cmd.onrender.com/';
+    window.open(mathLive, '_blank');
+  };
+  const handleClickPopLive = () => {
+    
+    const popLive = 'https://population-index.onrender.com/';
+    window.open(popLive, '_blank');
+  };
+
+  // const handleClickTodoLive = () => {
+    
+  //   const todoLive = 'https://github.com/Yidnekachew-cmd/math-magician';
+  //   window.open(todoLive, '_blank');
+  // };
+  
 
   return (
-    <>
+    <div className="w-full">
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route 
+        path="/" 
+        element={<Home 
+        handleClick={handleContactClick} 
+        handleDownloadClick={handleDownloadClick} 
+        handleLinkedinIconClick={handleLinkedinIconClick} 
+        handleGitHubIconClick={handleGitHubIconClick} 
+        handleEmailIconClick={handleEmailIconClick}
+        handleClickMathGitHub={handleClickMathGitHub}
+        handleClickPopGitHub={handleClickPopGitHub}
+        handleClickMathLive={handleClickMathLive}
+        handleClickPopLive={handleClickPopLive}
+        />} />
         <Route path="/About" element={<About />} />
         <Route path="/Experience" element={<Experience />} />
-        <Route path="/Projects" element={<Projects />} />
-        <Route path="/Contact" element={<Contact />} />
+        <Route 
+        path="/Projects" 
+        element={<Projects
+        handleClickMathGitHub={handleClickMathGitHub}
+        handleClickPopGitHub={handleClickPopGitHub}
+        handleClickMathLive={handleClickMathLive}
+        handleClickPopLive={handleClickPopLive} 
+        />} />
+        <Route 
+        path="/Contact" 
+        element={<Contact 
+        handleEmailIconClick={handleEmailIconClick} 
+        handleLinkedinIconClick={handleLinkedinIconClick} 
+        />} />
       </Routes>
-    </>
+      <Footer />
+    </div>
   )
 }
 
