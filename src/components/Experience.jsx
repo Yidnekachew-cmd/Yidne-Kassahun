6885114007dcdc34e99e1c99884b3bb4dce00cc5
Yidne <yamiyekn@gmail.com>
2024-01-14 13:11:31 +0300

    Create Experience component and add elements and styling.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,256 @@
+import '../index.css'
+import checkMark from '../assets/checkmark.png'
+// import arrow from '../assets/arrow.png'
 
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <div className="w-full bg-hero-bg bg-no-repeat bg-right-bottom bg-cover">
+    <div className="w-[95%] md:w-[80%] flex flex-col justify-center items-center mx-auto   h-auto pt-[15svh] pb-[8vh] md:pt-[8svh] lg:pt-[15svh]  lg:pb-[12vh]] ">
+    <p className="text-lg md:text-xl mb-2 text-[#A5ACB5]">Explore My</p>
+    <h1 className="text-3xl md:text-5xl font-bold pb-12">Experience</h1>
+      <div className="flex lg:flex-wrap flex-col lg:flex-row  justify-center items-center lg:items-start w-full space-y-12 lg:space-y-0 lg:space-x-8 mx-auto">
+        <div className="border-2 border-[#a3a3a3] bg-[fafafa] rounded-3xl p-6 shadow-2xl flex-1 h-auto w-[90%] lg:w-[40%]">
+          <h2 className="text-center text-2xl md:texl-3xl text-[#545454] font-bold pb-6">Frontend Development</h2>
+          <div className=" flex flex-wrap gap-10 items-start justify-around">
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>HTML</h3>
+                <p>Experienced</p>
+              </div>
+            </div>
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>CSS</h3>
+                <p>Experienced</p>
+              </div>
+            </div>
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>TailWindCss</h3>
+                <p>Experienced</p>
+              </div>
+            </div>
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>Bootstrap</h3>
+                <p>Experienced</p>
+              </div>
+            </div>
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>JavaScript</h3>
+                <p>Experienced</p>
+              </div>
+            </div>
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>TypeScript</h3>
+                <p>Intermediate</p>
+              </div>
+            </div>
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>React.js</h3>
+                <p>Experienced</p>
+              </div>
+            </div>
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>Redux Tool Kit</h3>
+                <p>Experienced</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="border-2 border-[#a3a3a3] bg-[fafafa] rounded-3xl p-6 shadow-2xl  flex-1 h-auto w-[90%] lg:w-[40%]">
+          <h2 className="text-center text-2xl md:texl-3xl text-[#545454] font-bold pb-6">Backend Development</h2>
+          <div className="flex flex-wrap gap-10 items-start justify-around">
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>PostgreSQL</h3>
+                <p>Intermediate</p>
+              </div>
+            </div>
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>MongoDb</h3>
+                <p>Intermediate</p>
+              </div>
+            </div>
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>Node JS</h3>
+                <p>Basic</p>
+              </div>
+            </div>
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>Ruby</h3>
+                <p>Intermediate</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="border-2 border-[#a3a3a3] bg-[fafafa] rounded-3xl p-6 shadow-2xl flex-1 h-auto w-[90%] lg:w-[40%]">
+          <h2 className="text-center text-2xl md:texl-3xl text-[#545454] font-bold pb-6">Tools</h2>
+          <div className="flex flex-wrap gap-10 items-start justify-around">
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>Git</h3>
+                <p>Experienced</p>
+              </div>
+            </div>
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>GitHub</h3>
+                <p>Experienced</p>
+              </div>
+            </div>
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>Rspec</h3>
+                <p>Intermediate</p>
+              </div>
+            </div>
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>JEST</h3>
+                <p>Intermediate</p>
+              </div>
+            </div>
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>Chrom Dev Tools</h3>
+                <p>Intermediate</p>
+              </div>
+           </div>
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>Vs Code</h3>
+                <p>Experienced</p>
+              </div>
+            </div>
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>npm</h3>
+                <p>Experienced</p>
+              </div>
+            </div>
+            <div className="flex items-start justify-center space-x-2 w-[40%]">
+              <img
+                src={checkMark}
+                alt="Experience icon"
+                className="w-[20%] h-[20%]"
+              />
+              <div>
+                <h3>Postman</h3>
+                <p>Intermediate</p>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+        
+      </div>
+    </div>
+    </div>
+  );
+};
 
 export default Experience
