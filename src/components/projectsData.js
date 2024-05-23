e9b04eb084b5bfec9fdee884c9fb56134716ab47
Yidne <Yamiyekn@gmail.com>
2024-05-23 22:34:54 +0300

    Add Population Index image.

--- src/components/projectsData.js
+++ src/components/projectsData.js
@@ -1,6 +1,6 @@
 import project1 from "../assets/GPT_3.png";
 import project2 from "../assets/image.png";
-import project3 from "../assets/Population-Index.png";
+import project3 from "../assets/Population-Index-1.png";
 const projectsData = [
   {
     id: 1,
