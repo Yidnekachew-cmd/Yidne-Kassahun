68534cd8dc76f5ba2f9880ba0f643c59e3da5fde
Yidne <Yamiyekn@gmail.com>
2024-05-24 10:20:03 +0300

    Add new project to projectDatas file.

--- src/components/projectsData.js
+++ src/components/projectsData.js
@@ -1,6 +1,7 @@
 import project1 from "../assets/GPT_3.png";
 import project2 from "../assets/image.png";
-import project3 from "../assets/Population-Index-1.png";
+import project3 from "../assets/Hoo-Bank.png";
+import project4 from "../assets/Population-Index-1.png";
 const projectsData = [
   {
     id: 1,
@@ -22,10 +23,19 @@ const projectsData = [
   },
   {
     id: 3,
+    title: "Modern UX/UI Hoo Bank",
+    description:
+      "A website that is used as a showcase to demonstrate my skills and abilities in building responsive and interactive web applications using react with Tailwind Css.",
+    image: project3,
+    live: "hoobank-zbaw.onrender.com/",
+    github: "https://github.com/Yidnekachew-cmd/HooBank/tree/dev",
+  },
+  {
+    id: 4,
     title: "Population Index",
     description:
       "Population Index is a powerful React-based SPA that allows you to explore detailed population metrics for countries all around the world!",
-    image: project3,
+    image: project4,
     live: "https://population-index.onrender.com/",
     github: "https://github.com/Yidnekachew-cmd/Popuation-Index",
   },
